d57c99cfb37362f85e3af94b7180d41c5d9ba98e
allandlo <allan.oliveira@aluno.faculdadeimpacta.com.br>
2020-09-10 00:12:12 -0300

    Initial webapp configuration.
    Angular starting setup, Routes and Router Guarding initial configuration. Bootstrap libraries and Google Font.
    
    Signed-off-by: allandlo <allan.oliveira@aluno.faculdadeimpacta.com.br>

---
+++ angular/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PageNoFoundComponent } from './page-no-found/page-no-found.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ServicesComponent } from './services/services.component';
+
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: '', component: HomeComponent, canActivate: [AuthGuard]},
+  {path: '**', component: PageNoFoundComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
