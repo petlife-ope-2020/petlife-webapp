a1356a083694f585f931512ae753c7f119306bdc
allandlo <allan.oliveira@aluno.faculdadeimpacta.com.br>
2020-09-10 00:30:01 -0300

    Angular authentication structure.
    Routes Guards and AuthServices for all components.
    
    Signed-off-by: allandlo <allan.oliveira@aluno.faculdadeimpacta.com.br>

---
+++ angular/src/app/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  isLoggedIn = true;
+
+  constructor() {}
+
+  AutenticateUser(): boolean {
+
+    return this.isLoggedIn;
+  }
+}
