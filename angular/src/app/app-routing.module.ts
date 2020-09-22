4323d9392b2f2a8c62709a1ea24ec613f49e9f87
allandlo <allan.oliveira@aluno.faculdadeimpacta.com.br>
2020-09-21 21:54:23 -0300

    ServiceComponent creation.
    Only ServiceComponent structure generation. No code added yet.
    
    Signed-off-by: allandlo <allan.oliveira@aluno.faculdadeimpacta.com.br>

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -10,6 +10,8 @@ import { ServicesComponent } from './services/services.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent, canActivate: [AuthGuard]},
+  {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
+  {path: 'services', component: ServicesComponent, canActivate: [AuthGuard]},
   {path: '', component: HomeComponent, canActivate: [AuthGuard]},
   {path: '**', component: PageNoFoundComponent},
 ];
