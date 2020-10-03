b6c7fe408ade798e5b565c6a0397a52aeaae1666
oould <oould@pop-os.localdomain>
2020-10-03 00:13:03 -0300

    Home and OrderRequests components tests are updated, HTML in both componenet as well. GetOrderInfo service created to OrderRequests components.

---
+++ angular/src/app/orders-requests/get-order-info.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetOrderInfoService {
+
+  private mockedInfomation = [
+    { 
+      'ClientName' : 'Allan',
+      'PetName' : 'Alberto',
+      'PetSpecie' : 'Cachorro',
+      'PetBreed' : 'Labrador',
+      'PetAge' : '10',
+      'Service' : 'Vacinação',
+      'Date' : 'Sexta',
+      'Time' : '18:00'
+    },
+    { 
+      'ClientName' : 'Eric',
+      'PetName' : 'Rex',
+      'PetSpecie' : 'Passarinho',
+      'PetBreed' : 'Papagaio',
+      'PetAge' : '3',
+      'Service' : 'Banho e Tosa',
+      'Date' : 'Sexta',
+      'Time' : '18:00'
+    },
+    { 
+      'ClientName' : 'Angela',
+      'PetName' : 'Ferdinando',
+      'PetSpecie' : 'Touro',
+      'PetBreed' : 'Corno',
+      'PetAge' : '14',
+      'Service' : 'Banho',
+      'Date' : 'Sexta',
+      'Time' : '18:00'
+    },
+    { 
+      'ClientName' : 'Isabela',
+      'PetName' : 'Fofinho',
+      'PetSpecie' : 'Gato',
+      'PetBreed' : 'Egipcio',
+      'PetAge' : '5',
+      'Service' : 'Banho e Tosa',
+      'Date' : 'Sexta',
+      'Time' : '18:00'
+    }
+  ];
+
+  constructor() { }
+
+  public GetInfo() {
+    return this.mockedInfomation;
+  };
+
+}
--- angular/src/app/orders-requests/orders-requests.component.spec.ts
+++ angular/src/app/orders-requests/orders-requests.component.spec.ts
@@ -1,25 +1,63 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrdersRequestsComponent } from './orders-requests.component';
+import { GetOrderInfoService } from './get-order-info.service';
 
 describe('OrdersRequestsComponent', () => {
   let component: OrdersRequestsComponent;
   let fixture: ComponentFixture<OrdersRequestsComponent>;
+  let serviceStub: Partial<GetOrderInfoService>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ OrdersRequestsComponent ]
-    })
-    .compileComponents();
-  }));
+  serviceStub = {
+    GetInfo : () => {
+      return[
+        { 
+          ClientName : 'Allan',
+          PetName : 'Alberto',
+          PetSpecie : 'Cachorro',
+          PetBreed : 'Labrador',
+          PetAge : '10',
+          Service : 'Vacinação',
+          Date : 'Sexta',
+          Time : '18:00'
+        },
+        { 
+          ClientName : 'Eric',
+          PetName : 'Rex',
+          PetSpecie : 'Passarinho',
+          PetBreed : 'Papagaio',
+          PetAge : '3',
+          Service : 'Banho e Tosa',
+          Date : 'Sexta',
+          Time : '18:00'
+        }
+      ]
+    }
+  };
 
   beforeEach(() => {
+
+    TestBed.configureTestingModule({
+      declarations: [ OrdersRequestsComponent ],
+      providers: [ {provide: GetOrderInfoService, useValue: serviceStub} ]
+    });
+
     fixture = TestBed.createComponent(OrdersRequestsComponent);
-    component = fixture.componentInstance;
+    component = fixture.debugElement.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get mockedData', () => {
+    expect(component.orders).toEqual(serviceStub.GetInfo());
+  });
+
+  it('should pass data from service to html page', () => {
+    const htmlElement = fixture.debugElement.nativeElement;
+    const content = htmlElement.querySelector('.col-8');
+    expect(content.textContent).toContain(component.orders[0]['ClientName']);
   });
 });
