e025d0c060a3500fec3914385ee731aaf4e89d5c
oould <oould@pop-os.localdomain>
2020-10-06 20:51:55 -0300

    Calendar component implemented into HomeComponent

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,7 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { ScrollingModule } from '@angular/cdk/scrolling';
-
+import { FullCalendarModule } from '@fullcalendar/angular'; // the main connector. must go first
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin
+import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
+import interactionPlugin from '@fullcalendar/interaction'; // a plugin
+import timeGridPlugin from '@fullcalendar/timegrid'; // a plugin
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
@@ -10,6 +14,14 @@ import { ProfileComponent } from './profile/profile.component';
 import { ServicesComponent } from './services/services.component';
 import { PageNoFoundComponent } from './page-no-found/page-no-found.component';
 import { OrdersRequestsComponent } from './orders-requests/orders-requests.component';
+
+
+FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+  dayGridPlugin,
+  resourceTimelinePlugin,
+  interactionPlugin,
+  timeGridPlugin
+]);
 
 @NgModule({
   declarations: [
@@ -24,7 +36,8 @@ import { OrdersRequestsComponent } from './orders-requests/orders-requests.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ScrollingModule
+    ScrollingModule,
+    FullCalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
