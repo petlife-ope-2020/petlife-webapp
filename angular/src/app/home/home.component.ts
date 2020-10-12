499f10d8b70a8ac03af453eb1f4a7469fe0b1157
oould <oould@pop-os.localdomain>
2020-10-12 18:46:10 -0300

    All changes made in this commit were to update the testes and component classes, updating methods names and adding the new dependecies to the tests.

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/angular';
+import { ScheduledOrdersService } from '../scheduled-orders/scheduled-orders.service';
 
 @Component({
   selector: 'app-home',
@@ -11,13 +12,17 @@ export class HomeComponent implements OnInit {
   private selectedEvent: any;
   private selectedEventId: any;
   private eventsArray = [
-    { id : '1', title: 'event 1', date: '2020-10-01' },
-    { id : '2', title: 'event 2', date: '2020-10-03' },
-    { id : '3', title: 'event 3', date: '2020-10-02' }
+    { id : '1', title: 'Consulta 1', start: '2020-10-06T12:30:00' },
+    { id : '2', title: 'Consulta 2', start: '2020-10-06T08:30:00' },
+    { id : '3', title: 'Consulta 3', start: '2020-10-07T12:30:00' }
   ]
+  private requestsArray: Array<any>;
 
-  constructor() { }
+  constructor(private scheduledOrders : ScheduledOrdersService) {  }
+  
   ngOnInit(): void {
+    this.requestsArray = this.scheduledOrders.getData();
+    this.createEvent();
   }
 
   calendarOptions: CalendarOptions = {
@@ -25,18 +30,18 @@ export class HomeComponent implements OnInit {
     selectable: true,
     weekends: true,
     events: this.eventsArray ,
-    schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
+    //schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
     headerToolbar: {
       left: 'dayGridMonth,timeGridWeek,timeGridDay',
       center: 'title',
-      right: 'deleteButton prevYear,prev,next,nextYear'
+      right: 'cancelButton prevYear,prev,next,nextYear'
     },
     footerToolbar: {
 
     },
     customButtons: {
-      deleteButton:{
-        text: 'Delete',
+      cancelButton:{
+        text: 'Cancelar',
         click: () => {
           this.removeEvent();
         }
@@ -48,7 +53,7 @@ export class HomeComponent implements OnInit {
     }
   };
 
-  removeEvent (){
+  removeEvent(){
     this.selectedEvent.remove();
     this.eventsArray.forEach(element => {
       if(element['id'] === this.selectedEventId){
@@ -56,6 +61,16 @@ export class HomeComponent implements OnInit {
         this.eventsArray.splice(elementIndex, 1);;
       }
     });
-    
   }
+
+  createEvent(){
+    this.requestsArray.forEach(element => {
+      if (element.schedule.confirmed) {
+        this.eventsArray.push(
+          { id : this.requestsArray.indexOf(element).toString(), title: element.client.name, start: element.schedule["date-time"]}
+        )
+      }
+    });  
+  }
+
 }
