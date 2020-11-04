c0981cb3ed42f279397db03f2f35fb63f03c4061
allandlo <allan.oliveira@aluno.faculdadeimpacta.com.br>
2020-11-03 23:48:37 -0300

    Server side routes setup
    Most of the routes path have been made.
    
    Signed-off-by: allandlo <allan.oliveira@aluno.faculdadeimpacta.com.br>

--- server/server.js
+++ server/server.js
@@ -1,13 +1,11 @@
 const express = require('express'),
       path = require('path'),
-      bodyParser = require('body-parser'),
       router = require('./routes/routes');
 
 const app = express(),
       port = process.env.PORT || 8080;
 
 app.use(router);
-app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('*', (req, res) => {
