dc387f92731e2f61a55ec01a756435f6c98e0974
Allan de Lima Oliveira <allanoliveira@Allans-Air.home>
2020-11-16 22:13:16 -0300

    Auth and cookies updated
    AuthService and cookies on profile and login were updated.
    
    Signed-off-by: Allan de Lima Oliveira <allanoliveira@Allans-Air.home>

--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
-import { CookieService } from 'ngx-cookie-service'
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-login',
