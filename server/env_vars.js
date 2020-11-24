73f1e1e32ee19aae6e0fb337716705ba976fe9b0
allandlo <allan.oliveira@aluno.faculdadeimpacta.com.br>
2020-11-23 23:20:14 -0300

    Server enviroment variables setup
    File env-example created and enviroment variables were setup for proper security and code management.
    
    Signed-off-by: allandlo <allan.oliveira@aluno.faculdadeimpacta.com.br>

---
+++ server/env_vars.js
@@ -0,0 +1,5 @@
+module.exports = {
+    usersApiBaseUrl: process.env.USERS_API_BASE_URL,
+    ordersApiBaseUrl: process.env.ORDERS_API_BASE_URL,
+    servicesApiBaseUrl: process.env.SERVICES_API_BASE_URL
+}
